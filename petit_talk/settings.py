e661232278383b586809c172a5742dc6aa1cde9f
saraelizalde <j7esg480@students.codeinstitute.net>
2025-11-05 18:45:37 +0000

    Add base layout and configure home app URLs and views

--- petit_talk/settings.py
+++ petit_talk/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'petit_talk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
