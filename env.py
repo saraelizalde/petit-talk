c91c3113cc97753e6cc3cf5e0c67222cbe6e3b91
saraelizalde <j7esg480@students.codeinstitute.net>
2025-11-19 17:49:26 +0000

    Remove env.py and add to gitignore

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault('DEBUG', 'True')
-os.environ.setdefault('SECRET_KEY', 'lEXuSg7uQqsh7EF5QPQYJ2b7GdhVCWuT')
-os.environ["DATABASE_URL"] = "postgresql://neondb_owner:npg_DgjfPbxi16ol@ep-purple-unit-a25gvv67.eu-central-1.aws.neon.tech/silo_river_width_374249"
-
-os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51QP9N9GLVhLhp5AKmLsnp1dcFQ9hQCWynRiKMBg2crxC1glpPOVSJbiNjvOwcSOQ5tRSOoi0OiaiIumErHsvnGqy00P75qyF0o"
-os.environ["STRIPE_SECRET_KEY"] = "sk_test_51QP9N9GLVhLhp5AKq7r3PAE5mWxKdXpBHHZlDAsLsbgjEKRpRJp880oqP80ZXMPMO6Iplv9SosRBHwYpkGztbVZT00xofe9eiQ"
-os.environ["STRIPE_WEBHOOK_SECRET"] = "whsec_z0Ci3XDShzCPi2G1jPg2VZ3CkYAyGnAS"
