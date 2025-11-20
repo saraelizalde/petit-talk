fc6ba1089d4a04429ecfe285329e25bcde14a65f
saraelizalde <j7esg480@students.codeinstitute.net>
2025-11-20 15:20:06 +0000

    Remove env.py from repo and add to .gitignore

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ.setdefault('DEBUG', 'True')
-os.environ.setdefault('SECRET_KEY', 'secret')
-os.environ["DATABASE_URL"] = "url"
-
-os.environ["STRIPE_PUBLIC_KEY"] = "test_public_placeholder"
-os.environ["STRIPE_SECRET_KEY"] = "test_secret_placeholder"
-os.environ["STRIPE_WEBHOOK_SECRET"] = "test_webhook_placeholder"
