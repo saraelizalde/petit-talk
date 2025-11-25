edd979e858938e27f6e23bfc18233f88ab52b96d
saraelizalde <j7esg480@students.codeinstitute.net>
2025-11-25 14:31:23 +0000

    Add active teachers to homepage teacher carousel section

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from offers.models import Offer
+from userprofile.models import Profile
 
 def index(request):
     active_offer = Offer.objects.filter(active=True).first()
+    teachers = Profile.objects.filter(is_teacher=True)
     context = {
         "active_offer": active_offer,
+        "teachers": teachers,
     }
     return render(request, 'home/index.html', context)
