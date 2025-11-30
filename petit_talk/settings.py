44ec11bec6d0b2922ecfa0fe7623d4ed71049f20
saraelizalde <j7esg480@students.codeinstitute.net>
2025-11-30 17:25:33 +0000

    Fix S3 media settings to use AWS S3 for media storage

--- petit_talk/settings.py
+++ petit_talk/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    # AWS
+    'storages',
     # Local apps
     'bookings',
     'checkout',
@@ -196,8 +198,24 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',  # map Django 'error' to Bootstrap 'danger'
 }
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = BASE_DIR / 'media'
+
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")  # ex: "eu-west-3"
+
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+# Very important: avoid ACL errors
+AWS_DEFAULT_ACL = None
+
+# Media files on S3
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+
 
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
